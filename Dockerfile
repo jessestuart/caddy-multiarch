e539fe3bb4429cd8ce2775d2aadd9ed0dc18f9a9
Jesse Stuart <hi@jessestuart.com>
2018-05-19 13:58:16 -0400

    Init commit -- new multiarch repo.

---
+++ Dockerfile
@@ -0,0 +1,67 @@
+ARG target
+
+# =======
+# Builder
+# =======
+FROM abiosoft/caddy:builder as builder
+
+COPY GOARCH /GOARCH
+
+COPY qemu-* /usr/bin/
+
+ARG plugins="git,filemanager,cors,realip,expires,cache,cloudflare"
+
+ARG goarch
+ENV GOARCH $goarch
+ENV GOROOT /usr/local/go
+ENV GOPATH /go
+ENV PATH "$GOROOT/bin:$GOPATH/bin:$GOPATH/linux_$GOARCH/bin:$PATH"
+
+# process wrapper
+RUN go get -v github.com/abiosoft/parent && \
+      (cp /go/bin/**/parent /bin/parent || \
+       cp -f /go/bin/parent /bin/parent) &>/dev/null
+
+RUN rm -rf /usr/bin/builder.sh
+COPY builder/builder.sh /usr/bin/builder.sh
+
+ARG version
+RUN VERSION=${version} PLUGINS=${plugins} GOARCH=${goarch} /bin/sh /usr/bin/builder.sh
+
+# ===========
+# Final stage
+# ===========
+FROM $target/alpine
+LABEL maintainer="Jesse Stuart <hi@jessestuart.com>"
+LABEL caddy_version="$version"
+
+ARG arch
+ENV ARCH=$arch
+
+COPY qemu-* /usr/bin/
+
+ENV GOPATH /go
+ENV PATH $PATH:$GOPATH/bin
+
+# Let's Encrypt Agreement
+ENV ACME_AGREE="true"
+
+RUN apk add --no-cache openssh-client git
+
+# install caddy
+COPY --from=builder /install/caddy /usr/bin/caddy
+
+# validate install
+RUN caddy -version && caddy -plugins
+
+EXPOSE 80 443 2015
+VOLUME /root/.caddy /srv
+WORKDIR /srv
+
+COPY Caddyfile /etc/Caddyfile
+
+# install process wrapper
+COPY --from=builder /bin/parent /bin/parent
+
+ENTRYPOINT ["/bin/parent", "caddy"]
+CMD ["--conf", "/etc/Caddyfile", "--log", "stdout", "--agree=$ACME_AGREE"]
---
+++ builder/builder.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+VERSION=${VERSION:-"0.11.0"}
+TELEMETRY=${ENABLE_TELEMETRY:-"true"}
+
+# caddy
+git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy \
+    && cd /go/src/github.com/mholt/caddy \
+    && git checkout -b "v$VERSION"
+
+# plugin helper
+GOOS=linux GOARCH=$GOARCH go get -v github.com/abiosoft/caddyplug/caddyplug
+if [ $GOARCH == 'amd64' ]; then
+  alias caddyplug="GOOS=linux GOARCH=$GOARCH $GOPATH/bin/caddyplug"
+else
+  alias caddyplug="GOOS=linux GOARCH=$GOARCH $GOPATH/bin/linux_$GOARCH/caddyplug"
+fi
+
+# telemetry
+run_file="/go/src/github.com/mholt/caddy/caddy/caddymain/run.go"
+line=$(awk '/const enableTelemetry = true/{print NR}' $run_file)
+if [ "$line" ] && [ $TELEMETRY = "false" ]; then
+    sed -i.bak -e "${line}d" $run_file
+    cat > "$run_file.disablestats.go" <<EOF
+    package caddymain
+    import "os"
+    var enableTelemetry = $TELEMETRY
+    func init() {
+        switch os.Getenv("ENABLE_TELEMETRY") {
+        case "0", "false":
+            enableTelemetry = false
+        case "1", "true":
+            enableTelemetry = true
+        }
+    }
+EOF
+fi
+
+# plugins
+for plugin in $(echo $PLUGINS | tr "," " "); do \
+  GOOS=linux GOARCH=$GOARCH go get -v $(caddyplug package $plugin); \
+  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
+      /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
+done
+
+# builder dependency
+git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
+
+# build
+cd /go/src/github.com/mholt/caddy/caddy \
+  && GOOS=linux GOARCH=$GOARCH go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
+  && mkdir -p /install \
+  && mv caddy /install
