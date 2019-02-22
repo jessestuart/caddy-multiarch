26d678d617041742dcf9ff3a2ec461e04098afe1
Jesse Stuart <hi@jessestuart.com>
2019-02-22 07:20:10 -0500

    [commit message skipped]

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
+export VERSION=${VERSION:-"0.11.0"}
 export TELEMETRY=${ENABLE_TELEMETRY:-"true"}
 
 export PATH="/usr/bin:$GOPATH/bin:$GOPATH/bin/linux_$GOARCH:$GOROOT/bin:$PATH"
 
 # caddy
-export CADDY_ROOT=/go/src/github.com/mholt/caddy
+CADDY_ROOT=/go/src/github.com/mholt/caddy
 mkdir -p $CADDY_ROOT
-git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy
+git clone https://github.com/mholt/caddy -b "$VERSION" /go/src/github.com/mholt/caddy
 cd /go/src/github.com/mholt/caddy || return 1
-git checkout -b "v${VERSION}"
+git checkout -b "${VERSION}"
+
+echo $PATH
 
 # plugin helper
 GOOS=linux GOARCH=${GOARCH} go get -v github.com/abiosoft/caddyplug/caddyplug
 GOOS=linux GOARCH=${GOARCH} go install -v github.com/abiosoft/caddyplug/caddyplug
+echo "caddyplug: $(which caddyplug)"
+command -v caddyplug
+ls -alh /usr/bin/
 alias caddyplug="GOOS=linux GOARCH=${GOARCH} caddyplug"
 
 # telemetry
@@ -36,16 +42,16 @@ alias caddyplug="GOOS=linux GOARCH=${GOARCH} caddyplug"
 # plugins
 for plugin in $(echo $PLUGINS | tr "," " "); do
   go get -v $(caddyplug package $plugin)
-  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" \
-    >/go/src/github.com/mholt/caddy/caddyhttp/$plugin.go
+  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" >/go/src/github.com/mholt/caddy/caddyhttp/$plugin.go \
+    ;
 done
 
 # builder dependency
 git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
 
 # build
-cd /go/src/github.com/mholt/caddy/caddy &&
-  GOOS=linux GOARCH=${GOARCH} go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM &&
+cd /go/src/github.com/mholt/caddy &&
   ls -alhR . &&
+  GOOS=linux GOARCH=${GOARCH} go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM &&
   mkdir -p /install &&
   mv caddy /install
