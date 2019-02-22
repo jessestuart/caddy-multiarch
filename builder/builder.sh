28a6936b56a1608f5adbdbdaeddb80d4ed46297c
Jesse Stuart <hi@jessestuart.com>
2019-02-22 03:51:44 -0500

    [commit message skipped]

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,28 +1,19 @@
 #!/bin/sh
 
-export VERSION=${VERSION:-"0.11.0"}
 export TELEMETRY=${ENABLE_TELEMETRY:-"true"}
 
 export PATH="/usr/bin:$GOPATH/bin:$GOPATH/bin/linux_$GOARCH:$GOROOT/bin:$PATH"
 
 # caddy
-CADDY_ROOT=/go/src/github.com/mholt/caddy
+export CADDY_ROOT=/go/src/github.com/mholt/caddy
 mkdir -p $CADDY_ROOT
 git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy
 cd /go/src/github.com/mholt/caddy || return 1
 git checkout -b "v${VERSION}"
 
-echo "==================================="
-echo "PATH:"
-echo $PATH
-echo "==================================="
-
 # plugin helper
 GOOS=linux GOARCH=${GOARCH} go get -v github.com/abiosoft/caddyplug/caddyplug
 GOOS=linux GOARCH=${GOARCH} go install -v github.com/abiosoft/caddyplug/caddyplug
-echo "caddyplug: $(which caddyplug)"
-command -v caddyplug
-ls -alh /usr/bin/
 alias caddyplug="GOOS=linux GOARCH=${GOARCH} caddyplug"
 
 # telemetry
@@ -45,16 +36,16 @@ alias caddyplug="GOOS=linux GOARCH=${GOARCH} caddyplug"
 # plugins
 for plugin in $(echo $PLUGINS | tr "," " "); do
   go get -v $(caddyplug package $plugin)
-  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" >/go/src/github.com/mholt/caddy/caddyhttp/$plugin.go \
-    ;
+  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" \
+    >/go/src/github.com/mholt/caddy/caddyhttp/$plugin.go
 done
 
 # builder dependency
 git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
 
 # build
-cd /go/src/github.com/mholt/caddy/caddy && \
-  GOOS=linux GOARCH=${GOARCH} go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM && \
-  ls -alhR . && \
-  mkdir -p /install && \
+cd /go/src/github.com/mholt/caddy/caddy &&
+  GOOS=linux GOARCH=${GOARCH} go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM &&
+  ls -alhR . &&
+  mkdir -p /install &&
   mv caddy /install
