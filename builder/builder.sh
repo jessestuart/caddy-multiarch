6e1e816feb6b27b51c14f53083efa281c06b04f4
Jesse Stuart <hi@jessestuart.com>
2019-04-15 10:04:24 -0400

    [commit message skipped]

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,53 +1,54 @@
 #!/bin/sh
 
-VERSION=${VERSION:-"0.11.0"}
+VERSION=${VERSION:-"0.11.5"}
 TELEMETRY=${ENABLE_TELEMETRY:-"true"}
 
-# caddy
-git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy \
-    && cd /go/src/github.com/mholt/caddy \
-    && git checkout -b "v$VERSION"
+# add `v` prefix for version numbers
+checkout_tag=false
+[ "$(echo $VERSION | cut -c1)" -ge 0 ] 2>/dev/null && VERSION="v$VERSION" && checkout_tag=true
 
 # plugin helper
 GOOS=linux GOARCH=$GOARCH go get -v github.com/abiosoft/caddyplug/caddyplug
 if [ $GOARCH == 'amd64' ]; then
-  alias caddyplug="GOOS=linux GOARCH=$GOARCH $GOPATH/bin/caddyplug"
+  alias caddyplug="GO111MODULE=off GOOS=linux GOARCH=$GOARCH $GOPATH/bin/caddyplug"
 else
-  alias caddyplug="GOOS=linux GOARCH=$GOARCH $GOPATH/bin/linux_$GOARCH/caddyplug"
+  alias caddyplug="GO111MODULE=off GOOS=linux GOARCH=$GOARCH $GOPATH/bin/linux_$GOARCH/caddyplug"
 fi
+
+# check for modules support
+go_mod=false
+[ -f /go/src/github.com/mholt/caddy/go.mod ] && export GO111MODULE=on && go_mod=true
 
 # telemetry
 run_file="/go/src/github.com/mholt/caddy/caddy/caddymain/run.go"
-line=$(awk '/const enableTelemetry = true/{print NR}' $run_file)
-if [ "$line" ] && [ $TELEMETRY = "false" ]; then
-    sed -i.bak -e "${line}d" $run_file
-    cat > "$run_file.disablestats.go" <<EOF
+if [ "$TELEMETRY" = "false" ]; then
+  cat >"$run_file.disablestats.go" <<EOF
     package caddymain
     import "os"
-    var enableTelemetry = $TELEMETRY
     func init() {
         switch os.Getenv("ENABLE_TELEMETRY") {
         case "0", "false":
-            enableTelemetry = false
+            EnableTelemetry = false
         case "1", "true":
-            enableTelemetry = true
+            EnableTelemetry = true
         }
     }
 EOF
 fi
 
 # plugins
-for plugin in $(echo $PLUGINS | tr "," " "); do \
-  GOOS=linux GOARCH=$GOARCH go get -v $(caddyplug package $plugin); \
-  printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
-      /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
+for plugin in $(echo $PLUGINS | tr "," " "); do
+  package=$(caddyplug package $plugin)
+  $go_mod || go get -v "$package" # not needed for modules
+  printf "package caddyhttp\nimport _ \"$package\"" >/go/src/github.com/mholt/caddy/caddyhttp/$plugin.go \
+    ;
 done
 
-# builder dependency
-git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
+# builder dependency, not needed for modules
+$go_mod || git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
 
 # build
-cd /go/src/github.com/mholt/caddy/caddy \
-  && GOOS=linux GOARCH=$GOARCH go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
-  && mkdir -p /install \
-  && mv caddy /install
+cd /go/src/github.com/mholt/caddy/caddy &&
+  GOOS=linux GOARCH=amd64 go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM &&
+  mkdir -p /install &&
+  mv caddy /install
